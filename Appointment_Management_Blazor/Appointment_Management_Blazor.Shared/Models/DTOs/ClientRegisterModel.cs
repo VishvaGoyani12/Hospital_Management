9cd53a4460c243831cb9d5ef39623c3a7ea8a2ab
VishvaGoyani12 <mt.vishva.goyani@gmail.com>
2025-06-10 10:19:23 +0530

    fix validation

--- Appointment_Management_Blazor/Appointment_Management_Blazor.Shared/Models/DTOs/ClientRegisterModel.cs
+++ Appointment_Management_Blazor/Appointment_Management_Blazor.Shared/Models/DTOs/ClientRegisterModel.cs
@@ -26,6 +26,7 @@ namespace Appointment_Management_Blazor.Shared.Models.DTOs
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        [Required]
         [DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
